b7bfae4a2716d7df080241d3811cc18b06a5bfb4
yufw <yynyygy@gmail.com>
2014-09-03 07:57:42 +0800

    Initial commit

---
+++ ex1_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, \tworld\n");
+    printf("hello, \vworld\n");
+    printf("hello, \rworld\n");
+    printf("hello, \f\rworld\n");
+
+    return 0;
+}
